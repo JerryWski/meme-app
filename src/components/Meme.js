4729e200cbae0f0dae03a0515f4bdc58299f6219
JerryWski <jarek_wasilewski@yahoo.com>
2023-03-12 23:23:36 +0100

    asyncs useeffct

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,22 +2,6 @@ import styles from "./Meme.module.css";
 import { useEffect, useState } from "react";
 
 const Meme = () => {
-  /**
-   * Challenge:
-   * As soon as the Meme component loads the first time,
-   * make an API call to "https://api.imgflip.com/get_memes".
-   *
-   * When the data comes in, save just the memes array part
-   * of that data to the `allMemes` state
-   *
-   * Think about if there are any dependencies that, if they
-   * changed, you'd want to cause to re-run this function.
-   *
-   * Hint: for now, don't try to use an async/await function.
-   * Instead, use `.then()` blocks to resolve the promises
-   * from using `fetch`. We'll learn why after this challenge.
-   */
-
   const [meme, setMeme] = useState({
     topText: "",
     bottomText: "",
@@ -25,13 +9,25 @@ const Meme = () => {
   });
 
   const [allMemeImages, setAllMemeImages] = useState([]);
-  
-  useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-    .then(res => res.json())
-    .then(data => setAllMemeImages(data.data.memes))
-  },[])
 
+   /**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
+
+  useEffect(() => {
+    async function getMemes() {
+      const res = await fetch("https://api.imgflip.com/get_memes");
+      const data = await res.json();
+      setAllMemeImages(data.data.memes);
+    }
+    getMemes();
+  }, []);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
